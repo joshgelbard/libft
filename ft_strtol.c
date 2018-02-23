e6868dce6af23eab78f36f8443636111346a05c9
Joshua Gelbard <jgelbard@e1z3r3p20.42.us.org>
2018-02-23 03:02:02 -0800

    ft_strtol (not yet using only ft funcs)

---
+++ ft_strtol.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtol.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/22 17:03:18 by jgelbard          #+#    #+#             */
+/*   Updated: 2018/02/23 02:54:52 by jgelbard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <string.h>
+#ifndef LONG_MAX
+# define LONG_MAX	9223372036854775807L
+# define LONG_MIN	(-LONG_MAX - 1L)
+#endif
+
+static int	valueof(char c, int base)
+{
+	if (base == 0 || !isalnum(c))
+		return (-1);
+	if (isdigit(c))
+		c = c - '0';
+	else if (isupper(c))
+		c = c - 'A' + 10;
+	else if (islower(c))
+		c = c - 'a' + 10;
+	return (c < base ? c : -1);
+}
+
+static int	get_base(const char *str, int base)
+{
+	if (base == 1 || base <= -1 || base > 36)
+		return (0);
+	else if (base == 0 && strncmp(str, "0x", 2) == 0)
+		return (16);
+	else if (base == 0 && *str == '0')
+		return (8);
+	else if (base == 0)
+		return (10);
+	else
+		return (base);
+}
+
+static void	handle_prefix(const char **str_p, int *base_p, int *neg_flag)
+{
+	char	*str;
+	int		base;
+
+	str = (char *)*str_p;
+	*neg_flag = 0;
+	while (isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+		*neg_flag = (*str++ == '-');
+	base = get_base(str, *base_p);
+	if (base == 16 && strncmp(str, "0x", 2) == 0 && valueof(str[2], 16) != -1)
+		str += 2;
+	*base_p = (valueof(*str, base) == -1) ? 0 : base;
+	*str_p = str;
+}
+
+long		clamp_to_bounds(unsigned long res, int isnegative)
+{
+	if (res > (unsigned long)LONG_MAX + isnegative)
+		return (isnegative ? LONG_MIN : LONG_MAX);
+	return (isnegative ? (long)(res * -1) : (long)res);
+}
+
+long		ft_strtol(const char *str, char **endptr, int base)
+{
+	unsigned long	place;
+	unsigned long	res;
+	unsigned long	tmp;
+	char			*end;
+	int				isnegative;
+
+	end = (char *)str;
+	handle_prefix(&str, &base, &isnegative);
+	if (base > 0)
+		end += (str - end);
+	while (*end && valueof(*end, base) != -1)
+		end += 1;
+	if (endptr && *endptr)
+		*endptr = end;
+	tmp = 0;
+	res = tmp;
+	place = 1;
+	while (--end >= str)
+	{
+		if ((tmp += valueof(*end, base) * place) < res || place < res)
+			return (isnegative ? LONG_MIN : LONG_MAX);
+		place *= base;
+		res = tmp;
+	}
+	return (clamp_to_bounds(res, isnegative));
+}
---
+++ test/test_strtol.c
@@ -0,0 +1,86 @@
+#include "../ft_strtol.c"
+#include <assert.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <strings.h>
+
+int	main(void)
+{
+	char *strs[] = {
+		"1",
+		"0",
+		"2x2x2",
+		"8",
+		"2147483647",
+		"2147483648",
+		"9223372036854775808",
+		"9223372036854775807",
+		"9223372036854775808      a",
+		"9223372036854775807 ueo",
+		"9223372036854775999",
+		"9223372036854775999",
+		"aaaED7aafffff123d",
+		"aoeoe10293xaoeu",
+		"3FaFaF",
+		NULL
+	};
+	char *prefixes[] = {
+
+		"+",
+		"-",
+		"0xr",
+		"1x",
+		"    ",
+		"   -",
+		"    +",
+
+		"    + 0x",
+		"    -   0x",
+		"a",
+		"0",
+		"0x",
+		"",
+		"+0",
+		"-0",
+		"      +0",
+		"     -0",
+		NULL
+	};
+	int bases[] = { 10, 0, 2, 36, 35, 16, 8, 1, -1, 37, 90000, -999 };
+	int i, j, k, base;
+	char **endptr1;
+	char **endptr2;
+	long res1, res2;
+	char str[90];
+	endptr1 = malloc(sizeof *endptr1);
+	endptr2 = malloc(sizeof *endptr2);
+	//endptr1 = (char **)NULL;
+	//endptr2 = (char **)NULL;
+	
+	i = -1;
+	while (strs[++i] != NULL)
+	{
+		j = -1;
+		while (bases[++j] != -999)
+		{
+			k = -1;
+			while (prefixes[++k] != NULL)
+			{
+				base = bases[j];
+				bzero(str, 90);
+				strcat(str, prefixes[k]);
+				strcat(str, strs[i]);
+				res1 = strtol(str, endptr1, base);
+				res2 = ft_strtol(str, endptr2, base);
+				if (res1 != res2)
+					printf("strtol(%s) --> %lu, %lu\n", str, res1, res2);
+				if (endptr1 && endptr2)
+					assert(strcmp(*endptr1, *endptr2) == 0);
+				assert(res1 == res2);
+			}
+		}
+	}
+	puts("ok");
+	return (0);
+}
