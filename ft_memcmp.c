a469f3c8c9a359f93fe8f67486ca39af968f2f59
Joshua Gelbard <jgelbard@e1z3r3p20.42.us.org>
2018-02-24 00:53:16 -0800

    Fix memcmp comparing 1 byte when asked for zero

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 13:30:57 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/02/24 00:30:52 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/02/24 00:51:46 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	if (!n)
+		return (0);
 	while (n-- && BYTE_AT(s1++) == BYTE_AT(s2++))
 		;
 	return (BYTE_AT(--s1) - BYTE_AT(--s2));
--- test/test_memcmp.c
+++ test/test_memcmp.c
@@ -10,6 +10,7 @@ int	main(void)
 	assert(ft_memcmp(apple, banana, 6) == memcmp(apple, banana, 6));
 	assert(ft_memcmp(banana, apple, 6) == memcmp(banana, apple, 6));
 	assert(ft_memcmp(banana, banana, 7) == memcmp(banana, banana, 7));
+	assert(ft_memcmp(banana + 1, apple + 1, 0) == memcmp(banana + 1, apple + 1, 0));
 	assert(ft_memcmp(unsgncheck, unsgncheck2, 9) == memcmp(unsgncheck, unsgncheck2, 9));
 	puts("ok");
 	return (0);
