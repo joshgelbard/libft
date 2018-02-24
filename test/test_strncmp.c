4024bdda31955850d7f5d9b4c26d2fef06807a50
Joshua Gelbard <jgelbard@e1z3r3p20.42.us.org>
2018-02-24 00:49:06 -0800

    Add strncmp and tests

---
+++ test/test_strncmp.c
@@ -0,0 +1,16 @@
+#include "ft_strncmp.h"
+
+int	main(void)
+{
+	char *s1 = "banana";
+	char *s2 = "banana";
+	char *s3 = "apple";
+	char *s4 = "";
+	assert(strncmp(s1, s2, 3) == ft_strncmp(s1, s2, 3));
+	assert(strncmp(s1, s3, 2) == ft_strncmp(s1, s3, 2));
+//	assert(strncmp(s3, s1, 0) == ft_strncmp(s3, s1, 0));
+	assert(strncmp(s1, s4, 5) == ft_strncmp(s1, s4, 5));
+	assert(strncmp(s4, s4, 12) == ft_strncmp(s4, s4, 12));
+	puts("ok");
+	return (0);
+}
