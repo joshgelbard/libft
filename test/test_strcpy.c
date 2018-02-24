b25f9f37d7bec9d8dbbfe40847c6017b8196ec8c
Joshua Gelbard <jgelbard@e1z3r3p20.42.us.org>
2018-02-23 21:30:03 -0800

    Add strcpy and tests

---
+++ test/test_strcpy.c
@@ -0,0 +1,17 @@
+#include "ft_strcpy.h"
+
+int	main(void)
+{
+	char str[] = "banana";
+	char str2[] = "";
+	char *dst = calloc(90, 1);
+	char *dst2 = calloc(90, 1);
+	ft_strcpy(dst2, str);
+	strcpy(dst, str);
+	assert(!strcmp(dst, dst2));
+	ft_strcpy(dst2, str2);
+	strcpy(dst, str2);
+	assert(!strcmp(dst, dst2));
+	puts("ok");
+	return (0);
+}
