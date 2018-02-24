14c40dc837f48ac196b27ae0121204c8343f44a5
Joshua Gelbard <jgelbard@e1z3r3p20.42.us.org>
2018-02-24 00:23:40 -0800

    Make memcmp short and readable

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,28 +6,16 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 13:30:57 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/02/20 22:40:46 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/02/24 00:30:52 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_memcmp.h"
+#define BYTE_AT *(unsigned char *)
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	int				d;
-	unsigned char	*b1;
-	unsigned char	*b2;
-
-	i = 0;
-	b1 = (unsigned char *)s1;
-	b2 = (unsigned char *)s2;
-	while (i < n)
-	{
-		d = b1[i] - b2[i];
-		if (d != 0)
-			return (d);
-		i++;
-	}
-	return (0);
+	while (n-- && BYTE_AT(s1++) == BYTE_AT(s2++))
+		;
+	return (BYTE_AT(--s1) - BYTE_AT(--s2));
 }
