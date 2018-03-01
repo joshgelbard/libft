e2440b4c9602abc39785eb3641f5964004db3afe
Joshua Gelbard <jgelbard@e1z3r5p5.42.us.org>
2018-03-01 13:49:48 -0800

    Return unsigned difference in strn?cmp

--- string/ft_strncmp.c
+++ string/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 15:43:22 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/02/25 00:20:42 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/03/01 13:48:10 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && n && (unsigned char)*s1 == (unsigned char)*s2)
+	while (*s1 && n && (unsigned char)*s1 == (unsigned char)*s2)
 	{
 		s1++;
 		s2++;
 		n--;
 	}
-	return (n == 0 ? 0 : *s1 - *s2);
+	return (n == 0 ? 0 : (unsigned char)*s1 - (unsigned char)*s2);
 }
