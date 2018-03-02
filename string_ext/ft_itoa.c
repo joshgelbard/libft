8f61360e1976a1a955b67d6d2efb36c6822c158f
Joshua Gelbard <jgelbard@e1z3r5p5.42.us.org>
2018-03-02 15:34:39 -0800

    Add working but lengthy itoa

--- string_ext/ft_itoa.c
+++ string_ext/ft_itoa.c
@@ -6,14 +6,57 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 14:05:07 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/03/02 14:19:40 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/03/02 15:33:53 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_string_ext.h"
+#include "ft_string.h"
+#include <string.h>
+
+static char	*itoa_str_init(unsigned int un, int wasnegative)
+{
+	size_t	cc;
+	char	*s;
+
+	cc = 1 + wasnegative;
+	while (un > 9)
+	{
+		++cc;
+		un /= 10;
+	}
+	s = ft_strnew(cc);
+	if (s)
+		while(cc)
+			s[--cc] = '0';
+	return (s);
+}
+
+static void	itoa_str_fill_pos(char *s, unsigned int un)
+{
+	size_t	i;
+
+	i = ft_strlen(s) - 1;
+	while (i)
+	{
+		s[i] += un % 10;
+		un /= 10;
+		--i;
+	}
+	s[i] += un;
+}
+
 char	*ft_itoa(int n)
 {
-	if (n == 0)
-		return "0";
-	else
-		return "1";
+	char			*s;
+	int				neg;
+
+	neg = (n < 0);
+	s = itoa_str_init((unsigned int)(neg ? -n : n), neg);
+	if (!s)
+		return (NULL);
+	itoa_str_fill_pos(s + neg, (unsigned int)(neg ? -n : n));
+	if (neg)
+		s[0] = '-';
+	return (s);
 }
--- test/string_ext/test_itoa.c
+++ test/string_ext/test_itoa.c
@@ -4,8 +4,8 @@
 int	main(void)
 {
 	int ints[] = { 0, 1, 11, 1234, 9090, 99999 };
-	//char *int_max_str = "2147483647";
-//	char *int_min_str = "-2147483648";
+	char *int_max_str = "2147483647";
+	char *int_min_str = "-2147483648";
 	char *strs[] = { "0", "1", "11", "1234", "9090", "99999", NULL };
 	char *neg = calloc(20, 1);
 	char *ans;
@@ -19,11 +19,12 @@ int	main(void)
 		strcat(neg, strs[i]);
 		ans = ft_itoa(ints[i]);
 		neg_ans = ft_itoa(ints[i] * -1);
-		printf("ft_itoa(%s) == %s and negative == %s\n", strs[i], ans, neg_ans);
 		assert(!strcmp(strs[i], ans));
 		assert(!strcmp(neg, neg_ans));
 		i++;
 	}
+	assert(!strcmp(int_max_str, ft_itoa(INT_MAX)));
+	assert(!strcmp(int_min_str, ft_itoa(INT_MIN)));
 	puts("itoa ok");
 	return(0);
 }
