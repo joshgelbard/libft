7455ec91d11d9323b79d82ac16ed85546a5f20b5
Joshua Gelbard <jgelbard@e1z3r5p5.42.us.org>
2018-03-02 13:56:38 -0800

    Make strsplit test less horrifying, add answer

--- string_ext/ft_strsplit.c
+++ string_ext/ft_strsplit.c
@@ -6,23 +6,69 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 22:31:58 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/03/01 22:59:55 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/03/02 13:53:47 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_string_ext.h"
+#include "ft_string.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+static int	word_count(char const *s, char delim)
+{
+	int	wc;
+	int	in_word;
+
+	wc = 0;
+	in_word = 0;
+
+	while (*s)
+	{
+		if (in_word && *s == delim)
+			in_word = 0;
+		else if (!in_word && *s != delim)
+		{
+			++wc;
+			in_word = 1;
+		}
+		++s;
+	}
+	return (wc);
+}
+
+static size_t	next_word_size(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && (s[i] == c) == (*s == c))
+		i++;
+	return (i);
+}
 
 char	**ft_strsplit(char const *s, char c)
 {
-	if (*s == 'x' || c == 'v')
-		return (0);
-	char **res = malloc(3 * sizeof(char *));
-	res[0] = (char *)malloc(4);
-	res[1] = (char *)malloc(2);
-	res[2] = (char *)malloc(6);
-	strcpy(res[0], "one");
-	strcpy(res[1], "2");
-	strcpy(res[2], "three");
-	return (res);
+	char	**ary;
+	int		wc;
+	size_t	ws;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	wc = word_count(s, c);
+	ary = (char **)malloc(wc * sizeof(char *) + 1);
+	if (!ary)
+		return (NULL);
+	i = 0;
+	while (i < wc)
+	{
+		ws = next_word_size(s, c);
+		if (*s != c)
+			ary[i++] = ft_strsub(s, 0, ws);
+		s += ws;
+	}
+	ary[i] = ft_strnew(0);
+	return (ary);
 }
--- test/string_ext/test_strsplit.c
+++ test/string_ext/test_strsplit.c
@@ -1,46 +1,65 @@
 #include "test.h"
 
-static int	test_many_combinations(void)
+void	print_was_wrong(char **wrong_ary, char **right_ary, int wrong_idx, char *og_str)
 {
-	char *first = "one";
-	char *second = "2";
-	char *third = "three";
+	printf("wrong answer for %s: res[%d] was %s but should have been %s (%p vs %p)\n",
+			og_str, wrong_idx, wrong_ary[wrong_idx], right_ary[wrong_idx], wrong_ary[wrong_idx], right_ary[wrong_idx]);
+}
+
+static int test_one(char *str, char **expect)
+{
+	char **res = ft_strsplit(str, '*');
+	int correct = 1;
+	for (int i = 0; expect[i][0] != '\0'; i++)
+		if (!(res[i]) || strcmp(res[i], expect[i]))
+		{
+			print_was_wrong(res, expect, i, str);
+			correct = 0;
+		}
+	return (correct);
+}
+
+void make_str(char *s, int i, int j, int k, int l)
+{
+	char *correct_answer[] = {
+		"one",
+		"2",
+		"three",
+		""
+	};
+	char *stars = "***";
+	strcat(s, ft_strsub(stars, 0, i));
+	strcat(s, correct_answer[0]);
+	strcat(s, ft_strsub(stars, 0, j));
+	strcat(s, correct_answer[1]);
+	strcat(s, ft_strsub(stars, 0, k));
+	strcat(s, correct_answer[2]);
+	strcat(s, ft_strsub(stars, 0, l));
+}
+
+int	main(void)
+{
+	char *if_one[] = {"one2three", ""};
+	char *if_twoA[] = {"one", "2three", ""};
+	char *if_twoB[] = {"one2", "three", ""};
+	char *if_three[] = {"one", "2", "three", ""};
 	char *s = calloc(90, 1);
-	char *stars = "****";
-	int	allcorrect = 1;
 	for (int i = 0; i < 3; i++)
 	for (int j = 0; j < 3; j++)
 	for (int k = 0; k < 3; k++)
 	for (int l = 0; l < 3; l++) // oh no
 	{
 		bzero(s, 90);
-		printf(".");
-		strcat(s, ft_strsub(stars, 0, i));
-		strcat(s, first);
-		strcat(s, ft_strsub(stars, 0, j));
-		strcat(s, second);
-		strcat(s, ft_strsub(stars, 0, k));
-		strcat(s, third);
-		strcat(s, ft_strsub(stars, 0, l));
-		char **res = ft_strsplit(s, '*');
-		if (strcmp(res[0], first)
-				|| strcmp(res[1], second)
-				|| strcmp(res[2], third))
-		{
-			allcorrect=0;
-			printf("split %s, got: %s, %s, %s\n", s, res[0], res[1], res[2]);
-		}
+		make_str(s, i, j, k, l);
+		if (!j && !k)
+			assert(test_one(s, if_one));
+		if (j && !k)
+			assert(test_one(s, if_twoA));
+		if (!j && k)
+			assert(test_one(s, if_twoB));
+		if (j && k)
+			assert(test_one(s, if_three));
 	}
-	return (allcorrect);
-}
-
-
-
-
-
-int	main(void)
-{
-	assert(test_many_combinations());
 	puts("strsplit ok");
-	return(0);
+	return (0);
 }
