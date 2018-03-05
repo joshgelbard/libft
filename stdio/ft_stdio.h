80fb16b799e781a5dd709d5b4d26c2e2cff4191e
Joshua Gelbard <jgelbard@e1z2r2p7.42.us.org>
2018-03-05 14:20:52 -0800

    Fix stdio prototypes

--- stdio/ft_stdio.h
+++ stdio/ft_stdio.h
@@ -6,7 +6,7 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 17:26:10 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/03/02 17:53:14 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/03/05 14:19:51 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putchar(char c);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putstr(char *s);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putendl(char *s);
-void	ft_puts(char *s);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putstr(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
+void	ft_puts(char const *s);
 #endif
