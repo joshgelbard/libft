b1da56b8d3801ff7f7fcd1ae64902a08a03a0396
Joshua Gelbard <jgelbard@e1z2r3p33.42.us.org>
2018-03-06 14:09:49 -0800

    Fix lstnew using t_list instead of t_list *

--- list/ft_lstnew.c
+++ list/ft_lstnew.c
@@ -6,8 +6,8 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 18:31:30 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/03/05 22:52:49 by jgelbard         ###   ########.fr       */
-/*                                                                            */
+/*   Updated: 2018/03/06 13:45:55 by jgelbard         ###   ########.fr       */
+/*                                                                           */
 /* ************************************************************************** */
 
 #include "ft_list.h"
@@ -15,7 +15,7 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list	node;
+	t_list	*node;
 
 	node = (t_list *)malloc(sizeof(t_list));
 	if (node == NULL)
@@ -24,7 +24,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		return (NULL);
 	}
 	node->next = NULL;
-	node->content = content;
+	node->content = (void *)content;
 	node->content_size = content ? content_size : 0;
-	return (&node);
+	return (node);
 }
