6716837aed7cba613c9d9916fc0f90878f9c0646
Joshua Gelbard <jgelbard@e1z2r5p36.42.us.org>
2018-03-05 22:10:06 -0800

    Add untested lstnew

---
+++ list/ft_lstnew.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/05 18:31:30 by jgelbard          #+#    #+#             */
+/*   Updated: 2018/03/05 22:17:01 by jgelbard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	node->content = content;
+	node->content_size = content ? content_size : 0;
+	return (&node);
+}
