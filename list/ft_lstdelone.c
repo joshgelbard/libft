6eca916036ca34f3971aa3f6994521c2b011dffb
Joshua Gelbard <jgelbard@e1z2r3p33.42.us.org>
2018-03-08 12:41:06 -0800

    Remove leftover printf in lstdelone

--- list/ft_lstdelone.c
+++ list/ft_lstdelone.c
@@ -6,17 +6,15 @@
 /*   By: jgelbard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 22:10:26 by jgelbard          #+#    #+#             */
-/*   Updated: 2018/03/06 14:45:31 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/03/08 12:40:38 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	printf("%p %p\n", alst, *alst);
 	if (alst && *alst)
 	{
 		del((*alst)->content, (*alst)->content_size);
